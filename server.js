14810f14582b29d75c239841f8e3be7649b66c81
Dave Hemmer <dave_hemmer@hotmail.com>
2017-12-23 02:55:52 -0800

    RIBD API Server complete to wonderCampers1.0

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const request = require('request');
+const async = require('async');
+const geocoder = require('geocoder');
 
 const app = express();
 
@@ -10,66 +12,230 @@ app.get('/api/info', (req, res) => {
 });
 
 app.get('/api/recareas', (req, res) => {
-  const param = req.query.state;
+   var state = req.query.state;
+   var pageOffset = 0;
+   var activity = req.query.activity;
+   var outOfData = false;
+   var myData = [];
+   var totalCount = 0;
 
-  if (!param) {
-    res.json({
-      error: 'Missing required parameter `state`',
-    });
-    return;
-  }
+   async.until(
+       function () { return (outOfData); },
+       function (callback) {
+         request({
+           url:'https://ridb.recreation.gov/api/v1/recareas.json',
+           qs:{
+             apikey:'5722E187D51D46678DC8F5B047FCB82E',
+             state: state,
+             offset: pageOffset*50,
+             full:true
+           }
+         },function(error,response,body) {
+           // console.log('response received ' + pageOffset);
+           pageOffset++;
+           if(!error && response.statusCode === 200) {
+             myData = myData.concat(JSON.parse(body).RECDATA);
+             totalCount = JSON.parse(body).METADATA.RESULTS.TOTAL_COUNT;
+             if (myData.length >= totalCount) {
+               // console.log('totalCount reached');
+               outOfData = true;
+             }
+             // setTimeout(callback, 5);
+             callback(null);
+           } else {
+             res.json({
+               error:error,
+               code:response.statusCode
+             });
+           }
 
-  request(`https://ridb.recreation.gov/api/v1/recareas.json?apikey=5722E187D51D46678DC8F5B047FCB82E&full=true&state=${param}`, function (error, response, body) {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    res.json(JSON.parse(body));
-  });
-
+         });
+       },
+       function (err) {
+         // console.log('async done');
+           myData.sort(sortRecareas);
+           res.json(myData);
+       }
+   );
 });
 
+app.get('/api/facilities', (req, res) => {
+  var facilityIDs = req.query.facilityIDs;
+  var state = req.query.state;
+  var query = req.query.query;
+  var radius = req.query.radius;
+  var placeName = req.query.placeName;
+  var myData = [];
+  var rqstParam = {};
 
-// app.get('/api/food', (req, res) => {
-//   const param = req.query.q;
+  if (!facilityIDs && !query && !radius) {
+    return false;
+  }
+  if (facilityIDs) {
+    async.each(facilityIDs, function(facilityID, callback) {
+      // console.log('Processing facility ',facilityID);
 
-//   if (!param) {
-//     res.json({
-//       error: 'Missing required parameter `q`',
-//     });
-//     return;
-//   }
+      request({
+        url:'https://ridb.recreation.gov/api/v1/facilities/'+facilityID.toString(),
+        qs:{
+            apikey:'5722E187D51D46678DC8F5B047FCB82E',
+            full:true
+        }
+      },function(error,response,body) {
+            if(!error && response.statusCode === 200) {
+              myData.push(JSON.parse(body));
+            } else {
+              callback('error: RidbController,FacilityID request');
+              // res.send({
+              //   error:error,
+              //   code:response.statusCode
+              // });
+            }
+            callback();
+          },function (err) {
+              // console.log('facilities DONE',myData.length);
+              myData.sort(sortFacilities);
+              res.send(myData);
+      });
+    }, function(err){
+        if( err ) {
+          console.log('ERR: RidbController,Async.FacilityIDs',err);
+        } else {
+          // console.log('facilities DONE',myData.length);
+          myData.sort(sortFacilities);
+          res.send(myData);
+        }
+    });
 
-//   // WARNING: Not for production use! The following statement
-//   // is not protected against SQL injections.
-//   const r = db.exec(`
-//     select ${COLUMNS.join(', ')} from entries
-//     where description like '%${param}%'
-//     limit 100
-//   `);
 
-//   if (r[0]) {
-//     res.json(
-//       r[0].values.map((entry) => {
-//         const e = {};
-//         COLUMNS.forEach((c, idx) => {
-//           // combine fat columns
-//           if (c.match(/^fa_/)) {
-//             e.fat_g = e.fat_g || 0.0;
-//             e.fat_g = parseFloat((
-//               parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-//             ).toFixed(2), 10);
-//           } else {
-//             e[c] = entry[idx];
-//           }
-//         });
-//         return e;
-//       }),
-//     );
-//   } else {
-//     res.json([]);
-//   }
-// });
+  }
+  if (query) {
+    // console.log('facilitiesQuery',query,state);
+    var outOfData = false;
+    var pageOffset = 0;
+    var totalCount = 0;
+
+    async.until(
+      function () { return (outOfData); },
+      function (callback) {
+        request({
+          url:'https://ridb.recreation.gov/api/v1/facilities.json',
+          qs:{
+            apikey:'5722E187D51D46678DC8F5B047FCB82E',
+            state: state,
+            query: query,
+            offset: pageOffset*50,
+            full:true
+          }
+        },function(error,response,body) {
+          // console.log('response received ',pageOffset,totalCount);
+          pageOffset++;
+          if(!error && response.statusCode === 200) {
+            myData = myData.concat(JSON.parse(body).RECDATA);
+            totalCount = JSON.parse(body).METADATA.RESULTS.TOTAL_COUNT;
+            if (myData.length >= totalCount) {
+              // console.log('totalCount reached');
+              outOfData = true;
+            }
+            // setTimeout(callback, 5);
+            callback(null);
+          } else {
+            res.send({
+              error:error,
+              code:response.statusCode
+            });
+          }
+
+        });
+      },
+      function (err) {
+        // console.log('async done');
+          myData.sort(sortFacilities);
+          res.send(myData);
+      }
+    );
+
+  }
+
+  if(radius && placeName) {
+    // console.log('radius & placeName',radius,placeName);
+    var coord = {};
+    geocoder.geocode(placeName+", "+state, function ( err, data ) {
+      if(typeof data.results[0] != 'undefined') {
+        coord.lat = data.results[0].geometry.location.lat;
+        coord.lng = data.results[0].geometry.location.lng;
+      }
+      // console.log('facilities :: radius',coord.lat,coord.lng);
+      var outOfData = false;
+      var pageOffset = 0;
+      var totalCount = 0;
+
+      async.until(
+        function () { return (outOfData); },
+        function (callback) {
+          request({
+            url:'https://ridb.recreation.gov/api/v1/facilities.json',
+            qs:{
+              apikey:'5722E187D51D46678DC8F5B047FCB82E',
+              state: state,
+              latitude: coord.lat,
+              longitude: coord.lng,
+              radius: radius,
+              offset: pageOffset*50,
+              full:true
+            }
+          },function(error,response,body) {
+            // console.log('response received ',pageOffset,totalCount);
+            pageOffset++;
+            if(!error && response.statusCode === 200) {
+              myData = myData.concat(JSON.parse(body).RECDATA);
+              totalCount = JSON.parse(body).METADATA.RESULTS.TOTAL_COUNT;
+              // console.log('response code 200 ',totalCount);
+              if (myData.length >= totalCount) {
+                // console.log('totalCount reached');
+                outOfData = true;
+              }
+              // setTimeout(callback, 5);
+              callback(null);
+            } else {
+              res.send({
+                error:error,
+                code:response.statusCode
+              });
+            }
+
+          });
+        },
+        function (err) {
+          // console.log('async done');
+          myData.sort(sortFacilities);
+          // console.log('myData.length',myData.length);
+          res.send(myData);
+        }
+      );
+
+    }); // geocoder
+  }
+});
+
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
+
+
+function sortRecareas(a,b) {
+  if (a.RecAreaName < b.RecAreaName)
+    return -1;
+  if (a.RecAreaName > b.RecAreaName)
+    return 1;
+  return 0;
+}
+
+function sortFacilities(a,b) {
+  if (a.FacilityName < b.FacilityName)
+    return -1;
+  if (a.FacilityName > b.FacilityName)
+    return 1;
+  return 0;
+}
